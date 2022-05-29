64b828851f85aab38ac82d99cf51bac26613b7a7
jvcypriano@gmail.com <jvcypriano@gmail.com>
2022-05-29 20:52:12 +0000

    converting transformation scripts to use delta lake

--- transform/1. race_results.py
+++ transform/1. race_results.py
@@ -22,30 +22,30 @@ v_file_date = dbutils.widgets.get("p_file_date")
 
 # COMMAND ----------
 
-races_df = spark.read.parquet(f"{processed_folder_path}/races") \
+races_df = spark.read.format("delta").load(f"{processed_folder_path}/races") \
 .withColumnRenamed("name", "race_name") \
 .withColumnRenamed("date", "race_date")
 
 # COMMAND ----------
 
-circuits_df = spark.read.parquet(f"{processed_folder_path}/circuits") \
+circuits_df = spark.read.format("delta").load(f"{processed_folder_path}/circuits") \
 .withColumnRenamed("location", "circuit_location")
 
 # COMMAND ----------
 
-drivers_df = spark.read.parquet(f"{processed_folder_path}/drivers") \
+drivers_df = spark.read.format("delta").load(f"{processed_folder_path}/drivers") \
 .withColumnRenamed("name", "driver_name") \
 .withColumnRenamed("number", "driver_number") \
 .withColumnRenamed("nationality", "driver_nationality")
 
 # COMMAND ----------
 
-constructors_df = spark.read.parquet(f"{processed_folder_path}/constructors") \
+constructors_df = spark.read.format("delta").load(f"{processed_folder_path}/constructors") \
 .withColumnRenamed("name", "team")
 
 # COMMAND ----------
 
-results_df = spark.read.parquet(f"{processed_folder_path}/results") \
+results_df = spark.read.format("delta").load(f"{processed_folder_path}/results") \
 .filter(f"file_date = '{v_file_date}'") \
 .withColumnRenamed("time", "race_time") \
 .withColumnRenamed("race_id", "result_race_id") \
@@ -90,4 +90,5 @@ final_df = add_ingestion_date(selected_df)
 
 # COMMAND ----------
 
-overwrite_partition(final_df, 'f1_presentation', 'race_results', 'race_id')
+merge_condition = "tgt.driver_name = src.driver_name AND tgt.race_id = src.race_id"
+merge_delta_data(final_df, 'f1_presentation', 'race_results', presentation_folder_path, merge_condition, 'race_id')
--- transform/2. driver_standings.py
+++ transform/2. driver_standings.py
@@ -26,7 +26,7 @@ from pyspark.sql.functions import sum, when, col, count
 
 # COMMAND ----------
 
-race_results_df = spark.read.parquet(f"{presentation_folder_path}/race_results") \
+race_results_df = spark.read.format("delta").load(f"{presentation_folder_path}/race_results") \
 .filter(f"file_date = '{v_file_date}'")
 
 # COMMAND ----------
@@ -35,13 +35,13 @@ race_year_list = df_column_to_list(race_results_df, 'race_year')
 
 # COMMAND ----------
 
-race_results_df = spark.read.parquet(f"{presentation_folder_path}/race_results") \
+race_results_df = spark.read.format("delta").load(f"{presentation_folder_path}/race_results") \
 .filter(col("race_year").isin(race_year_list))
 
 # COMMAND ----------
 
 driver_standings_df = race_results_df \
-.groupBy("race_year", "driver_name", "driver_nationality", "team") \
+.groupBy("race_year", "driver_name", "driver_nationality") \
 .agg(sum("points").alias("total_points"),
     count(when(col("position") == 1, True)).alias("wins"))
 
@@ -55,4 +55,5 @@ final_df = driver_standings_df.withColumn("rank", rank().over(driver_rank_spec))
 
 # COMMAND ----------
 
-overwrite_partition(final_df, 'f1_presentation', 'driver_standings', 'race_year')
+merge_condition = "tgt.driver_name = src.driver_name AND tgt.race_year = src.race_year"
+merge_delta_data(final_df, 'f1_presentation', 'driver_standings', presentation_folder_path, merge_condition, 'race_year')
