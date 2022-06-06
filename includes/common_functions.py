333927a013f618e03c7f17e6a30a11cd36fe8af9
jvcypriano@gmail.com <jvcypriano@gmail.com>
2022-06-06 23:24:55 +0000

    Commenting the functions used.

--- includes/common_functions.py
+++ includes/common_functions.py
@@ -1,12 +1,29 @@
 # Databricks notebook source
 from pyspark.sql.functions import current_timestamp
 def add_ingestion_date(input_df):
+    """Adds a ingestion_date column to a DataFrame with the current timestamp.
+    
+    Args:
+        input_df: DataFrame that you wish to add the ingestion_date column.
+    
+    Returns:
+        DataFrame equal to the input_df with the ingestion_date column added.
+        """
     output_df = input_df.withColumn("ingestion_date", current_timestamp())
     return output_df
 
 # COMMAND ----------
 
 def re_arrange_partition_column(input_df, partition_column):
+    """Re-arranges the DataFrame so that the column that the data is to be partitioned by is positioned as the last one.
+    
+    Args:
+        input_df: Base DataFrame that needs the columns to be re-arranged.
+        partition_column: Column that the data will be partitioned by.
+        
+    Returns:
+        DataFrame with the columns re-arranged.
+        """
     column_list = []
     for column_name in input_df.schema.names:
         if column_name != partition_column:
@@ -18,6 +35,17 @@ def re_arrange_partition_column(input_df, partition_column):
 # COMMAND ----------
 
 def overwrite_partition(input_df, db_name, table_name, partition_column):
+    """Uses the overwrite mode to write data as parquet files and DataBase tables.
+    
+    Args:
+        input_df: DataFrame that is to be written as parquet files and DB Table.
+        db_name: String containing the name of the DataBase that the data will be written into.
+        table_name: String containing the name of the Table that the data will be written into.
+        partition_column: Column that will be the one to partition the data by.
+        
+    Returns:
+        None.
+        """
     output_df = re_arrange_partition_column(input_df, partition_column)
     spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
     if (spark._jsparkSession.catalog().tableExists(f"{db_name}.{table_name}")):
@@ -28,6 +56,15 @@ def overwrite_partition(input_df, db_name, table_name, partition_column):
 # COMMAND ----------
 
 def df_column_to_list(input_df, column_name):
+    """Creates a list with the distinct values in a column in a DataFrame.
+    
+    Args:
+        input_df: DataFrame that is to be searched.
+        column_name: String containing the name of the column you want to get the distinct values.
+        
+    Returns:
+        column_value_list: list containing all distinct values in the column.
+        """
     df_row_list = input_df.select(column_name) \
     .distinct() \
     .collect()
@@ -38,6 +75,19 @@ def df_column_to_list(input_df, column_name):
 # COMMAND ----------
 
 def merge_delta_data(input_df, db_name, table_name, folder_path, merge_condition, partition_column):
+    """Writes a DataFrame's data into the delta lake, using incremental load.
+    
+    Args:
+        input_df: DataFrame with the data to be written into the delta lake.
+        db_name: String containing the name of the DataBase that the data will be written into.
+        table_name: String containing the name of the Table that the data will be written into.
+        folder_path: Path to the folder that the delta file will be written into.
+        merge_condition: String containing the merge conditions.
+        partition_column: Column that the data will be partitioned by.
+        
+    Returns:
+        None
+        """
     spark.conf.set("spark.databricks.optimizer.dynamicPartitionPruning", "true")
 
     from delta.tables import DeltaTable
